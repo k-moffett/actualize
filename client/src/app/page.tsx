1f93d3ac62cc6244fc4587757810dcc812deaa83
Kurt <kurtis.moffett@newwestern.com>
2023-05-28 11:37:50 -0700

    Created initial client and server. Added scripts to start client server and a postgres db.

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata} from 'next';
+
+const metadata: Metadata = {
+    title: 'Actualize',
+};
+
+export default function Root() {
+    return (
+        <main>
+      ok
+        </main>
+    );
+}
