cb821d6d826ecd453ceb659c516ab6658aae85ec
Kurt <kurtis.moffett@newwestern.com>
2023-05-28 23:46:47 -0700

    Removed comment.

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -11,6 +11,4 @@ export const userApi = createApi({
     }),
 });
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const { useGetUserQuery } = userApi;
